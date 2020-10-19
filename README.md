cad6ebb8af619e7f20852a1ba79ab0f02eaf6873
Jorge Diaz Montoya <jdzm@outlook.es>
2020-10-19 15:28:42 -0500

    Startup project

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# **Firebase Google Storage with Spring**
+
+## A simple application file uploads to storage firebase.
---
+++ src/main/java/ec/com/dev/StorageFirebaseJavaApplication.java
@@ -0,0 +1,13 @@
+package ec.com.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageFirebaseJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageFirebaseJavaApplication.class, args);
+	}
+
+}
